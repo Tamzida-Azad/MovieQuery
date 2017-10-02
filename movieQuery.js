bd682822fe93c6a9837e8c18028aec9257212e31
nz881 <zamannafi3@gmail.com>
2017-10-02 09:17:10 +0600

    Assignment 05

--- movieQuery.js
+++ movieQuery.js
@@ -4,20 +4,29 @@ var movieList = [
   { title: "Omen : The Final Conflict", year: "1981", length: 108, rating: "6.7", genre: "horror" },
   { title: "Gladiator", year: "2000", length: 155, rating: "8.5", genre: "historical" },
   { title: "The Patriot", year: "2000", length: 165, rating: "7.1", genre: "historical" },
-  { title: "The Matrix Reloaded”, year: “2003", length: 138, rating: "7.2", genre: "sci-fi" }
+  { title: "The Matrix Reloaded”, year: “2003", length: 138, rating: "7.2", genre: "sci-fi" },
+  { title: "The Cronicoles of Narnia”, year: “2005", length: 150, rating: "8.0", genre: "adventure" },
+  { title: "Ghost Ship”, year: “2009", length: 138, rating: "7.2", genre: "horror" }
+
 ];
 
 function getMovieTitle( index ){
   //add code
+  return movieList[index].title;
 }
 
 console.log(getMovieTitle(3));
+console.log(getMovieTitle(0));
+console.log(getMovieTitle(1));
 
 function addMovie( movie ){
   //add code
+ movieList.push(movie);
+ return movieList;
+
 }
 
-var newMovie = {};
+var newMovie = {tittle:"Grudge”, year: “2011", length: 138, rating: "7.8", genre: "horror"};
 console.log(addMovie(newMovie));
 
 function movieByRating(){
